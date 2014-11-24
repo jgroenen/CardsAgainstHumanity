4c44519534e4b8d17a5bcc055064dc990635b6a1
Johan Groenen <johan@mediainjection.com>
2014-11-24 15:15:57 +0100

    favicon

--- index.php
+++ index.php
@@ -2,6 +2,8 @@
 <html>
     <head>
         <meta name='viewport' content='width=320,initial-scale=1,user-scalable=0'>
+		<link rel='image_src' href='http://www.p1x.nl/assets/img/CAH.png'/>
+		<link rel='shortcut icon' type='image/png' href='http://www.p1x.nl/assets/img/CAH.png'>
         <title>Cards Against Humanity</title>
         <link rel="stylesheet" href="/assets/css/reset.css">
         <link rel="stylesheet" href="/assets/css/cah.css">
