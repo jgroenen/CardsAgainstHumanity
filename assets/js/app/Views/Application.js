14175edf84e4665d7ebae9af3c5d2235d2889f3f
Johan Groenen <johan@mediainjection.com>
2014-11-24 18:01:25 +0100

    expansion packs

--- assets/js/app/Views/Application.js
+++ assets/js/app/Views/Application.js
@@ -3,8 +3,9 @@ define([
     'Collections/Questions',
     'Collections/Answers',
     'text!templates/application.tmpl',
-    'Views/Cards'
-], function (settings, QuestionsCollection, AnswersCollection, template, CardsView) {
+    'Views/Cards',
+    'cards'
+], function (settings, QuestionsCollection, AnswersCollection, template, CardsView, cards) {
     return Backbone.View.extend({
         template: _.template(template),
         
@@ -12,9 +13,27 @@ define([
         },
 
         initialize: function () {
-            this.questions = new QuestionsCollection(settings.cards.black);
+            //this.questions = new QuestionsCollection(settings.cards.black);
+            this.questions = new QuestionsCollection(cards
+                .filter(function (card) {
+                    return card.cardType === "Q";
+                })
+                .map(function (card) {
+                    card.question = card.text;
+                    return card;
+                })
+            );
             this.questions.reset(this.questions.shuffle(), {silent:true});
-            this.answers = new AnswersCollection(settings.cards.white);
+            //this.answers = new AnswersCollection(settings.cards.white);
+            this.answers = new AnswersCollection(cards
+                .filter(function (card) {
+                    return card.cardType === "A";
+                })
+                .map(function (card) {
+                    card.answer = card.text;
+                    return card;
+                })
+            );
             this.answers.reset(this.answers.shuffle(), {silent:true});
             this.cardsView = new CardsView({
                 answers: this.answers,
