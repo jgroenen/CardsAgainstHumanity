7e959c8b93e7b8a3907c19d0ebbd872ab3d7cf1a
Johan Groenen <johan@mediainjection.com>
2014-11-24 15:36:02 +0100

    analytics

--- index.php
+++ index.php
@@ -5,9 +5,12 @@
         <link rel='image_src' href='http://www.p1x.nl/assets/img/CAH.png'/>
         <link rel='shortcut icon' type='image/png' href='http://www.p1x.nl/assets/img/CAH.png'>
         <title>Cards Against Humanity</title>
+        <meta name="title" content="Cards Against Humanity Mobile Web App">
+        <meta name="description" content="Cards Against Humanity is a party game for horrible people. Unlike most of the party games you've played before, Cards Against Humanity is as despicable and awkward as you and your friends.">
         <link rel="stylesheet" href="/assets/css/reset.css">
         <link rel="stylesheet" href="/assets/css/cah.css">
-	<script src="/assets/js/vendor/require/require-2.1.15.min.js" data-main="/assets/js/app/bootstrap.js"></script>
+        <script src="/assets/js/vendor/require/require-2.1.15.min.js" data-main="/assets/js/app/bootstrap.js"></script>
+        <script src="google_analytics.js"></script>
     </head>
     <body></body>
 </html>
